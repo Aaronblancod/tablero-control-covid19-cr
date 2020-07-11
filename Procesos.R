dc1a56cc024063b98524766f6b6ace3589eb41e7
Aaronblancod <Aaronblancod@gmail.com>
2020-07-10 23:16:06 -0600

    02/07/2020

--- Procesos.R
+++ Procesos.R
@@ -38,7 +38,7 @@ archivo_positivos_cantones <- 'https://raw.githubusercontent.com/geoprocesamient
 archivo_activos_cantones <- 'https://raw.githubusercontent.com/geoprocesamiento-2020i/datos/master/covid19/ms/07_02_CSV_ACTIVOS.csv'
 archivo_recuperados_cantones <- 'https://raw.githubusercontent.com/geoprocesamiento-2020i/datos/master/covid19/ms/07_02_CSV_RECUP.csv'
 archivo_fallecidos_cantones <- 'https://raw.githubusercontent.com/geoprocesamiento-2020i/datos/master/covid19/ms/07_02_CSV_FALLECIDOS.csv'
-archivo_activos_distritos <- 'https://raw.githubusercontent.com/geoprocesamiento-2020i/datos/master/covid19/ms/07_02_CSV_GENERAL_DISTRITOS.csv'
+archivo_general_distritos <- 'https://raw.githubusercontent.com/geoprocesamiento-2020i/datos/master/covid19/ms/07_02_CSV_GENERAL_DISTRITOS.csv'
 
 
 # Data frame de datos generales por país
@@ -49,47 +49,94 @@ df_general_pais_ultima_fecha <-
   df_general_pais %>%
   filter(FECHA == max(FECHA, na.rm = TRUE))
 
-# Data frame de datos generales por distrtito
-df_general_distrito <- read.csv(archivo_activos_distritos, sep = caracter_separador)
-df_general_distrito$FECHA <- as.Date(df_general_distrito$X.2, "%d/%m/%Y")
-# Data frame de datos generales por distrito en la última fecha
-df_general_distrito_ultima_fecha <- 
-  df_general_distrito %>%
-  filter(X.2 == max(X.2, na.rm = TRUE))
+#--------------- Datos activos por distrito --------------------------------
 
-
-
-
-
+# Carga del archivo CSV en un data frame
+df_general_distritos_sucio <- read.csv(archivo_general_distritos)
+# Eliminación de filas y columnas que corresponden a encabezados, totales, etc.
+df_general_distritos_ultima_fecha <- df_general_distritos_sucio[-c(1:5), -c(1, 3, 10, 11)]
+# Cambio de nombre de las columnas
+df_general_distritos_ultima_fecha <- 
+  df_general_distritos_ultima_fecha %>%
+  rename(provincia = X.1,
+         canton = X.3,
+         distrito = X.4,
+         positivos = X.5,
+         recuperados = X.6,
+         fallecidos = X.7,
+         activos = X.8
+  ) %>%  
+  mutate_all(funs(sub("^\\s*$", NA, .))) %>% # Se llenan con NA las celdas con espacios vacíos
+  mutate(distrito = if_else(distrito == "El Carmen", "Carmen", distrito)) %>%
+  mutate(distrito = if_else(distrito == "Valle de La Estrella", "Valle La Estrella", distrito)) %>%
+  mutate(distrito = if_else(distrito == "La Amistad", "La  Amistad", distrito)) %>%
+  fill(c(1,2)) # Se rellenan "hacia abajo" las columnas de provincia y cantón con valor NA
+# Borrado de las filas con valor de NA o de "Sin información de distrito" en la columna de distrito
+df_general_distritos_ultima_fecha <- df_general_distritos_ultima_fecha[!is.na(df_general_distritos_ultima_fecha$distrito), ]
+df_general_distritos_ultima_fecha <- df_general_distritos_ultima_fecha[df_general_distritos_ultima_fecha$distrito != 'Sin información de distrito', ]
+# Conversión a integer de los tipos de datos de las columnas con cifras
+df_general_distritos_ultima_fecha$positivos <- as.integer(df_general_distritos_ultima_fecha$positivos)
+df_general_distritos_ultima_fecha$recuperados <- as.integer(df_general_distritos_ultima_fecha$recuperados)
+df_general_distritos_ultima_fecha$fallecidos <- as.integer(df_general_distritos_ultima_fecha$fallecidos)
+df_general_distritos_ultima_fecha$activos <- as.integer(df_general_distritos_ultima_fecha$activos)
 # Objeto sf de distritos
-url_base_wfs_ign_5mil <- "http://geos.snitcr.go.cr/be/IGN_5/wfs?"
-solicitud_wfs_ign_5mil_distrito <- "request=GetFeature&service=WFS&version=2.0.0&typeName=limitedistrital_5k&outputFormat=application/json"
-sf_disitrito <-
-  st_read(paste0(url_base_wfs_ign_5mil, solicitud_wfs_ign_5mil_distrito)) %>%
+sf_distritos <- st_read('https://raw.githubusercontent.com/geoprocesamiento-2020i/datos/master/delimitacion-territorial-administrativa/cr/ign/cr_limite_distrital_ign_wgs84.geojson')
+# Objeto sf de casos positivos en distritos en la última fecha
+sf_general_distritos_ultima_fecha <-
+  left_join(sf_distritos, df_general_distritos_ultima_fecha, by = c('provincia', 'canton', 'distrito'))
+```
+#------------------------------------- Mapa de calor -------------------------------------------------------
+paleta_azul <- colorBin(palette = "Blues", domain = sf_positivos_cantones_ultima_fecha$positivos, 
+                        bins = 10)
+m1 <- leaflet(sf_positivos_cantones_ultima_fecha) %>% 
+  fitBounds(lng1 = -86, lng2 = -82, lat1 = 8, lat2 = 11) %>%
+  addProviderTiles(providers$Esri.WorldTopoMap, group = "Esri World TopoMap") %>%
+  addProviderTiles(providers$CartoDB.DarkMatter, group = "CartoDB Dark") %>%
+  addPolygons(fillColor = ~paleta_azul(positivos), stroke=T, fillOpacity = 1,
+              color="black", weight=0.2, opacity= 0.5, group = "Cantones",
+              popup = paste("Provincia: ", sf_positivos_cantones_ultima_fecha$provincia, "<br>",
+                            "Cantón: ", sf_positivos_cantones_ultima_fecha$canton, "<br>",
+                            "Positivos: ", sf_positivos_cantones_ultima_fecha$positivos)) %>%
+  addLegend("bottomright", pal = paleta_azul, values = ~positivos, title = "Casos positivos", 
+            opacity = 1)%>%  
+  addLayersControl(baseGroups = c("Esri World TopoMap", "CartoDB Dark"), overlayGroups = c("Cantones"),
+                   options = layersControlOptions(collapsed = TRUE))%>%  
+  addMiniMap(toggleDisplay = TRUE, tiles = providers$Esri.WorldImagery, position = "bottomleft")
+m1
+
+leaflet(spdf) %>%
+  addProviderTiles(providers$Thunderforest.TransportDark) %>%
+  addWebGLHeatmap(size=25,units='px')
+
+library(leaflet.extras)
+
+heatmap <- leaflet(sf_general_distritos_ultima_fecha) %>%
+  addProviderTiles(providers$CartoDB.DarkMatter) %>%
+  addWebGLHeatmap(lng=~long, lat=~lat, intensity = ~mag, size=60000)
+heatmap
+
+  
+#borrar
+
+# Data frame de casos positivos por cantón
+df_positivos_cantones_ancho <- read.csv(archivo_positivos_cantones, sep = caracter_separador)
+df_positivos_cantones <-
+  df_positivos_cantones_ancho %>%
+  pivot_longer(cols = c(-cod_provin, -provincia, -cod_canton, -canton), names_to = "fecha", values_to = "positivos")
+df_positivos_cantones$fecha <- as.Date(df_positivos_cantones$fecha, "X%d.%m.%Y")
+
+# Data frame de casos positivos por cantón en la última fecha
+df_positivos_cantones_ultima_fecha <- 
+  df_positivos_cantones %>%
+  filter(fecha == max(fecha, na.rm = TRUE)) %>%
+  select(cod_canton, positivos)
+
+sf_cantones <-
+  st_read(paste0(url_base_wfs_ign_5mil, solicitud_wfs_ign_5mil_cantones)) %>%
   st_simplify(dTolerance = 1000) %>%
   st_transform(4326)
 
-
-#--------------- Datos activos por distrito --------------------------------
-
-# Data frame de casos activos por cantón
-df_activos_distritos_ancho <- read.csv(archivo_activos_distritos, sep = caracter_separador)
-df_activos_distritos <- df_activos_distritos_ancho %>%
-  pivot_longer(cols = c(-cod_provin, -provincia, -codigo_dta, -distrito), 
-               names_to = "fecha", values_to = "activos")
-df_activos_distritos$fecha <- as.Date(df_activos_distritos$fecha, "X%d.%m.%Y")
-
-# Data frame de casos activos por cantón en la última fecha
-df_activos_cantones_ultima_fecha <- 
-  df_activos_cantones %>%
-  filter(fecha == max(fecha, na.rm = TRUE)) %>%
-  select(cod_canton, activos)
-
-# Objeto sf de casos activos en cantones en la última fecha
-sf_activos_cantones_ultima_fecha <-
-  left_join(sf_cantones, df_activos_cantones_ultima_fecha, by = c('cod_canton')) %>%
-  arrange(desc(activos))
-
-
-
-
+# Objeto sf de casos positivos en cantones en la última fecha
+sf_positivos_cantones_ultima_fecha <-
+  left_join(sf_cantones, df_positivos_cantones_ultima_fecha, by = c('cod_canton')) %>%
+  arrange(desc(positivos))
